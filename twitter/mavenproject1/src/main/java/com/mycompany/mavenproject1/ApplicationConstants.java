bbf5d2fe96ac6f12ccfc6f015cdbfe3c85489234
saufisyafiq <36787191+saufisyafiq@users.noreply.github.com>
2018-06-04 03:32:24 +0800

    Add files via upload

--- twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/ApplicationConstants.java
+++ twitter/mavenproject1/src/main/java/com/mycompany/mavenproject1/ApplicationConstants.java
@@ -1,18 +1,27 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.mavenproject1;
 
-
+/**
+ *
+ * @author hp
+ */
 public class ApplicationConstants {
  
-    
-    public static final String CONSUMER_KEY_KEY = "G1vsi5YJdGqxAeeatvoQ9X3UB";
-    public static final String CONSUMER_SECRET_KEY = "6S4FN4D4mkbdB5bLjeIBVwowRPT8JuIooSBM9iYvUVqLCZ7onW";
-    public static final String ACCESS_TOKEN_KEY = "******-w46iJSxP8Pwo6xYiYANu49lJJnhz5i";
-    public static final String ACCESS_TOKEN_SECRET_KEY = "FzEjaKg1tkJ9vpm2mGTLFUnG6yT36ZpciBvTLSuORZqtG";
+    // Twitter application specific secret constants KEY WAS REMOVED TO PROTECT DEVELOPER TWITTER ACCOUNT https://apps.twitter.com/.   
+    public static final String CONSUMER_KEY_KEY = "[INSERT CONSUMER KEY HERE]";
+    public static final String CONSUMER_SECRET_KEY = "[INSERT TWITTER DEVELOPER CONSUMER SECRET KEY]";
+    public static final String ACCESS_TOKEN_KEY = "[INSERT ACCESS TOKEN KEY]";
+    public static final String ACCESS_TOKEN_SECRET_KEY = "[INSERT ACCESS TOKEN SECRET KEY]";
  
+    // constants
     public static final String NOT_AVAILABLE = "Not Available";
     public static final String EMPTY = "";
  
+    // Topology Constants 
     public static final String TOPOLOGY_NAME = "twitter-topology";
     public static final String TWITTER_SPOUT_ID = "twitterSpout";
     public static final String DETAILS_BOLT_ID = "detailsExtractorBolt";
